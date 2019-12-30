a1c922f1321dfd2d65d7f7d8c81a6cf95ec72890
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-12-31 02:14:44 +0900

    feat: about and posts pages added

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>This is about page</h1>
+    <p>this is a sample</p>
+    <p>this is a sample</p>
+    <p>this is a sample</p>
+    <p>this is a sample</p>
+  </div>
+</template>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
