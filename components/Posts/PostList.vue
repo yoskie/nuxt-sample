48d07096794b01ea56d8a130b9fd887dbc0521bf
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-12-31 15:05:17 +0900

    feat: admin_post_form added

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
       title="Hello there!"
       previewText="This is my first post"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
       title="Hello there!"
       previewText="This is my first post2"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
       title="Hello there!"
       previewText="This is my first post3"
@@ -27,6 +30,12 @@
   export default {
     components: {
       PostPreview: PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
     }
   }
 
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+  import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+  export default {
+    components: {
+      AdminPostForm
+    },
+    data() {
+      return {
+        loadedPost: {
+          author: 'Sample',
+          title: 'Test',
+          content: 'test',
+          thumbnailLink: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
+</style>
