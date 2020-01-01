1458da1a43a4883529c8db393ee3477637e14e41
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-01-02 01:55:53 +0900

    feat: fetching data setting revised

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -4,12 +4,15 @@
 
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
 
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
 
     <AppControlInput
       control-type="textarea"
       v-model="editedPost.content">Content</AppControlInput>
 
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.previewText">PreviewText</AppControlInput>
     <AppButton type="submit">Save</AppButton>
 
     <AppButton
@@ -42,8 +45,9 @@
           : {
             author: '',
             title: '',
-            thumbnailLink: '',
-            content: ''
+            thumbnail: '',
+            content: '',
+            previewText: ''
             }
       };
     },
